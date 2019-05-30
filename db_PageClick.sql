fc08fea935237099029d39e103be317a70bf353c
HSX <44840686+HSXaaa@users.noreply.github.com>
2019-05-30 12:44:56 +0800

    数据库设计

---
+++ db_PageClick.sql
@@ -0,0 +1,23 @@
+﻿CREATE DATABASE db_PageClick;
+
+USE `db_PageClick`;
+
+DROP TABLE IF EXISTS `indexpage`;
+
+CREATE TABLE `indexpage` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `indexcount` int(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+insert  into `indexpage`(`id`,`indexcount`) values (1,1);
+
+DROP TABLE IF EXISTS `websitepage`;
+
+CREATE TABLE `websitepage` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `websitecount` int(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+insert  into `websitepage`(`id`,`websitecount`) values (1,1);
